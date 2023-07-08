90c5e829a950c3851d7241a98db22e6131e723b9
hitmorecode <38550613+hitmorecode@users.noreply.github.com>
2023-07-08 20:53:41 +0200

    updated script.js

--- js/script.js
+++ js/script.js
@@ -1,19 +1,16 @@
 const score = document.querySelectorAll(".btn-score");
 const btnSubmit = document.querySelector(".btn-submit");
-const select = document.querySelector("#select");
-const imgSubmited = document.querySelectorAll(".score-submited");
-const first = document.querySelector(".first");
 const scoredNumber = document.querySelector(".scored-number");
 const maxScore = document.querySelector(".max-score");
+const card = document.querySelector(".card-show");
+const cardRated = document.querySelector(".card-rated");
 
 maxScore.textContent = score.length;
 
-function giveStars () {
+function giveStars (num) {
   for (let i = 0; i < score.length; i++) {
     score[i].addEventListener ("click", function () {
-      let starValue = score[i].innerText;
-
-      /* if there is no active class */
+      let num = score[i].innerText;
 
       let current = document.getElementsByClassName("active");
 
@@ -22,37 +19,17 @@ function giveStars () {
       }
       this.className += (" active");
 
-      scoredNumber.textContent = starValue;
-
-      /* ############################################################################# */
-      
-      /* if active class already exists */
-      /*
-      let current = document.getElementsByClassName("active");
-      
-      current[0].className = current[0].className.replace("active", "");
-      this.className += " active"; 
-      */
-
-      /* ############################################################################## */
-
-      /* if there is no active class */
-      
-      // document.querySelector(".active")?.classList.remove("active");
-      // score[i].classList.add("active");
-      // this.classList.add("active");
+      scoredNumber.textContent = num;
+ 
     });
   }
+  return num;
 }
 
 let valueOfScore = giveStars();
 
-for (let i = 0; i < imgSubmited.length; i++) {
-  btnSubmit.addEventListener ("click", function () {
-    imgSubmited[i].classList.add("hide");
-    imgSubmited[i].classList.toggle("hide")
-    first.classList.add("hide");
-    select.classList.add("hide");
-  });
-}
+btnSubmit.addEventListener ("click", function () {
+  card.classList.add("hide");
+  cardRated.classList.add("show");
+});
 
